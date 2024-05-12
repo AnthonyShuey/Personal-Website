e9b94af832e693cf06ec2cf094851c9f6d9c8baf
AnthonyShuey <87520912+AnthonyShuey@users.noreply.github.com>
2024-05-12 11:16:11 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white text-black">
       <Head>
         <title>Your Personal Website</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className="bg-gray-100 p-4 flex justify-end space-x-4">
+      <nav className="bg-white p-4 flex justify-end space-x-4">
         <a href="#home" className="text-black hover:text-blue-500">Home</a>
         <a href="#favorites" className="text-black hover:text-blue-500">Favorites</a>
         <a href="#projects" className="text-black hover:text-blue-500">Projects</a>
@@ -19,7 +19,7 @@ export default function Home() {
 
       <main className="flex-grow p-6 flex flex-col items-center justify-center text-center">
         <h1 className="text-4xl font-bold">Hello!</h1>
-        <p>I&apos;m Anthony Shuey. I love math, computer science, everything in between. I&apos;m a student at Boston University pursuing a double major in math and computer science. I love solving competitive programming/math problems, and I love learning new technologies!</p>
+        <p>I&apos;m Anthony Shuey. I love math, computer science, and everything in between. I&apos;m a student at Boston University pursuing a double major in math and computer science. I love solving competitive programming/math problems, and I love learning new technologies!</p>
         <Image src="/image_for_website.jpg" alt="Your Image" width={200} height={200} className="rounded-full mt-4"/>
         <div className="flex mt-2">
           <a href="https://www.linkedin.com/in/anthonshuey" target="_blank" rel="noopener noreferrer">
@@ -34,7 +34,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="bg-gray-200 p-4 flex justify-around">
+      <footer className="bg-white p-4 flex justify-around">
         <div>
           <h2 className="font-bold">I am currently exploring</h2>
           <ul>
