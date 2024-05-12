7a1d63b4e52a4ecee64a485733ca86f9798fd4eb
AnthonyShuey <87520912+AnthonyShuey@users.noreply.github.com>
2024-05-12 10:19:59 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,10 @@ export default function Home() {
         <p>I&apos;m Anthony Shuey. I love math, computer science, everything in between. I&apos;m a student at Boston University pursuing a double major in math and computer science. I love solving competitive programming/math problems, and I love learning new technologies!</p>
         <Image src="/image_for_website.jpg" alt="Your Image" width={200} height={200} className="rounded-full mt-4"/>
         <a href="https://www.linkedin.com/in/anthonshuey">
-          <Image src="/linkedin_logo.png" alt="LinkedIn Logo" width={30} height={30}/>
+          <Image src="/linkedin_logo.jpg" alt="LinkedIn Logo" width={30} height={30}/>
         </a>
-        <a href="https://www.instagram.com/yourusername">
-          <Image src="/instagram_logo.png" alt="Instagram Logo" width={30} height={30}/>
+        <a href="https://www.instagram.com/shueyjanthony/">
+          <Image src="/instagram_logo.jpg" alt="Instagram Logo" width={30} height={30}/>
         </a>
       </main>
 
