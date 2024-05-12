3a742f1a46ceda57e9904da9fcb51c91832c3d0a
AnthonyShuey <87520912+AnthonyShuey@users.noreply.github.com>
2024-05-12 13:20:24 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import './Home.module.css';
 
 export default function Home() {
     return (
-        <div className={flexContainer + " flex-col min-h-screen bg-white text-black"}>
+        <div className={.flexContainer + " flex-col min-h-screen bg-white text-black"}>
             <Head>
                 <title>Your Personal Website</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <nav className={flexContainer + " bg-white p-4 justify-end space-x-4"}>
+            <nav className={.flexContainer + " bg-white p-4 justify-end space-x-4"}>
                 <a href="#home" className="text-black hover:text-blue-500">Home</a>
                 <a href="#favorites" className="text-black hover:text-blue-500">Favorites</a>
                 <a href="#projects" className="text-black hover:text-blue-500">Projects</a>
@@ -18,7 +18,7 @@ export default function Home() {
                 <a href="#writing" className="text-black hover:text-blue-500">Writing</a>
             </nav>
 
-            <main className={flexContainer + " flex-grow p-6 flex-col items-center justify-center text-center"}>
+            <main className={.flexContainer + " flex-grow p-6 flex-col items-center justify-center text-center"}>
                 <h1 className="text-4xl font-bold">Hello!</h1>
                 <p>I&apos;m Anthony Shuey. I love math, computer science, and everything in between. I&apos;m a student at Boston University pursuing a double major in math and computer science. I love solving competitive programming/math problems, and I love learning new technologies!</p>
                 <Image src="/image_for_website.jpg" alt="Your Image" width={200} height={200} className="rounded-full mt-4"/>
@@ -35,7 +35,7 @@ export default function Home() {
                 </div>
             </main>
 
-            <footer className={flexContainer + " bg-white p-4 justify-around"}>
+            <footer className={.flexContainer + " bg-white p-4 justify-around"}>
                 <div>
                     <h2 className="font-bold">I am currently exploring</h2>
                     <ul>
