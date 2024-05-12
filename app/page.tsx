f0f8b8ce3c8b9af93a5ce99a3881391146399a36
AnthonyShuey <87520912+AnthonyShuey@users.noreply.github.com>
2024-05-12 13:34:51 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from './Home.module.css';  // This imports the CSS module
+import styles from './Home.module.css'; // Ensure this is correctly importing your CSS module
 
 export default function Home() {
     return (
-        <div className={styles.flexContainer + " flex-col min-h-screen bg-white text-black"}>
+        <div className={`${styles.flexContainer} flex-col min-h-screen bg-white text-black`}>
             <Head>
                 <title>Your Personal Website</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <nav className={styles.flexContainer + " bg-white p-4 justify-end space-x-4"}>
+            <nav className={`${styles.flexContainer} bg-white p-4 justify-end space-x-4`}>
                 <a href="#home" className="text-black hover:text-blue-500">Home</a>
                 <a href="#favorites" className="text-black hover:text-blue-500">Favorites</a>
                 <a href="#projects" className="text-black hover:text-blue-500">Projects</a>
@@ -18,11 +18,11 @@ export default function Home() {
                 <a href="#writing" className="text-black hover:text-blue-500">Writing</a>
             </nav>
 
-            <main className={styles.flexContainer + " flex-grow p-6 flex-col items-center justify-center text-center"}>
+            <main className={`${styles.flexContainer} flex-grow p-6 flex-col items-center justify-center text-center`}>
                 <h1 className="text-4xl font-bold">Hello!</h1>
-                <p>I'm Anthony Shuey. I love math, computer science, and everything in between. I'm a student at Boston University pursuing a double major in math and computer science. I love solving competitive programming/math problems, and I love learning new technologies!</p>
+                <p>I&apos;m Anthony Shuey. I love math, computer science, and everything in between. I&apos;m a student at Boston University pursuing a double major in math and computer science. I love solving competitive programming/math problems, and I love learning new technologies!</p>
                 <Image src="/image_for_website.jpg" alt="Your Image" width={200} height={200} className="rounded-full mt-4"/>
-                <div className={styles.flexContainer + " mt-2 items-center"}>
+                <div className={`${styles.flexContainer} mt-2 items-center`}>
                     <a href="https://www.linkedin.com/in/anthonshuey" target="_blank" rel="noopener noreferrer" className={styles.logoLink}>
                         <Image src="/linkedin_logo.jpg" alt="LinkedIn Logo" width={30} height={30} className="mx-1"/>
                     </a>
@@ -35,7 +35,7 @@ export default function Home() {
                 </div>
             </main>
 
-            <footer className={styles.flexContainer + " bg-white p-4 justify-around"}>
+            <footer className={`${styles.flexContainer} bg-white p-4 justify-around`}>
                 <div>
                     <h2 className="font-bold">I am currently exploring</h2>
                     <ul>
@@ -54,6 +54,7 @@ export default function Home() {
         </div>
     );
 }
+
 
 
 
